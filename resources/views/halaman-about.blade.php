2679846dae767038e8b59e354894be678e49e021
Aqilafadilla25 <aqila24ti@mahasiswa.pcr.ac.id>
2025-09-29 13:46:18 +0700

    Initial commit

---
+++ resources/views/halaman-about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>About Page</title>
+</head>
+<body>
+    <h1>Ini adalah halaman about</h1>
+    <p>Laravel membuat development lebih cepat </p>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\MatakuliahController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/pcr', function(){
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+
+Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+
+Route::get('/nama/{param1}', function($param1){
+    return 'Nama saya: '.$param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1 = ""){
+    return 'Nim saya: '.$param1;
+});
+
+Route::get('/about', function(){
+    return view('halaman-about');
+});
+
+
+Route::get('/matakuliah', [MatakuliahController::class, 'index']);
+
+Route::get('/matakuliah/show/{kode?}', [MatakuliahController::class, 'show']);
+
