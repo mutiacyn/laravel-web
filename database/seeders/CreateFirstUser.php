d557984e9cc8010390e9f6e906db6d88bc3b7f38
mutiacyn <mutia24ti@mahasiswa.pcr.ac.id>
2025-11-17 15:29:02 +0700

    p11

--- database/seeders/CreateFirstUser.php
+++ database/seeders/CreateFirstUser.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace Database\Seeders;
-use App\Models\User;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class CreateFirstUser extends Seeder
 {
